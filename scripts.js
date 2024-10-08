6b5b46eb836d2c6ddad35898965e0a828697a825
IgorKntt <igor.gorskin@hotmail.com>
2024-10-08 16:14:27 +0500

    JS script editted

--- scripts.js
+++ scripts.js
@@ -5,20 +5,17 @@ const headerNavBurger = document.getElementById("headerNavBurger");
 const mobileMenu = document.getElementById("mobileMenu");
 const headerSlider = document.getElementById("headerSlider");
 
-document.addEventListener("DOMContentLoaded", () => {
+mobileMenuClose.onclick = () => {
+  mobileMenu.style.display = "none";
+};
 
-  mobileMenuClose.onclick = () => {
-    mobileMenu.style.display = "none";
-  };
+headerNavBurger.onclick = () => {
+  mobileMenu.style.display = "block";
+}
 
-  headerNavBurger.onclick = () => {
-    mobileMenu.style.display = "block";
-  }
+headerSlider.addEventListener("click", e => {
+  if (e.target.tagName != "A") return;
 
-  headerSlider.addEventListener("click", e => {
-    if (e.target.tagName != "A") return;
-
-    headerSlider.querySelectorAll("li").forEach((element) => element.className = "");
-    e.target.parentNode.className = "active";
-  })
-});
+  headerSlider.querySelectorAll("li").forEach((element) => element.className = "");
+  e.target.parentNode.className = "active";
+})
