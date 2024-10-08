d6cc55ceb046b1126afcb703192753572b4f8b88
IgorKntt <igor.gorskin@hotmail.com>
2024-10-08 11:02:16 +0500

    Mobile menu interactions added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log('js file connected')
+console.log('js file connected');
+
+const mobileMenuClose = document.getElementById("mobileMenuClose");
+const headerNavBurger = document.getElementById("headerNavBurger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMContentLoaded")
+
+  mobileMenuClose.onclick = () => {
+    mobileMenu.style.display = "none";
+  };
+
+  headerNavBurger.onclick = () => {
+    mobileMenu.style.display = "block";
+  }
+});
